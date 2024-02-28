32b67f50f8aa0d41856ffa72af89df18be072f05
ole196D3V <72858960+ole196D3V@users.noreply.github.com>
2024-02-28 22:09:27 +0700

    Delete MebKemTorg2.0 directory

--- MebKemTorg2.0/js/script.js
+++
@@ -1,7 +0,0 @@
-let burger = document.querySelector('.burger-menu')
-let nav = document.querySelector('.main-nav')
-
-themeButton.onclick = function() {
-  page.classList.toggle('lightheme');
-  page.classList.toggle('darktheme');
-}
