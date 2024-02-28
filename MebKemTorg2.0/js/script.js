aaabbf0f523d79cbfe7ee271a7b7e7b4a912ad10
ole196D3V <72858960+ole196D3V@users.noreply.github.com>
2024-02-28 22:08:00 +0700

    Add files via upload

---
+++ MebKemTorg2.0/js/script.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.burger-menu')
+let nav = document.querySelector('.main-nav')
+
+themeButton.onclick = function() {
+  page.classList.toggle('lightheme');
+  page.classList.toggle('darktheme');
+}
